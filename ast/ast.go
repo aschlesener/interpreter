661dc387f79227ad9c3ae7cb08671402cd0d3a3b
aschlesener <amy@amyschlesener.com>
2022-09-07 22:12:56 -0700

    Add AST and start on parser

---
+++ ast/ast.go
@@ -0,0 +1,59 @@
+package ast
+
+import "interpreter/token"
+
+/*
+	ast.go provides an Abstract Syntax Tree for the Monkey language.
+	It is used for parsing tokens.
+
+	Example: the Monkey source code "let x = 5;" would be represented by the following AST:
+
+*/
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		// return the root node of our AST
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
